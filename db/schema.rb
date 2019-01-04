f374a66e3100384a1cb7089aa45bd2ab29a6dc5f
Briana Phelps <briana.phelps@icloud.com>
2019-01-03 20:25:57 -0800

    added decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_041650) do
+ActiveRecord::Schema.define(version: 2018_12_30_013925) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_041650) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
-    t.string "price"
+    t.decimal "price"
   end
 
 end
